3b68a8cb91bb4b830b062232005a1e0f256f3734
Jonathan Poisson <Poisson.Jonathan777@gmail.com>
2016-02-28 14:35:45 +0000

    Finished final part of DS project 1. Need to add functionality for variables and test

--- datastructures/assignments/project1/calculator.cpp
+++ datastructures/assignments/project1/calculator.cpp
@@ -6,11 +6,32 @@
 
 #include <iostream>
 #include <cstring>
+#include <math.h>
 #include <stack>
 #include <stdlib.h>
 #include <queue>
 
 using namespace std;
+
+/* Struct to help with postfixFunctions */
+struct postfixHelper {
+    /* Returns precedence if input is operator, otherwise returns -1 */
+    static int isOperator(string input) {
+        if (input == "(")      {return 0;}
+        else if (input == ")") {return 0;}
+        else if (input == "^") {return 3;}
+        else if (input == "*") {return 2;}
+        else if (input == "/") {return 2;}
+        else if (input == "+") {return 1;}
+        else if (input == "-") {return 1;}
+        else { return -1;} // Input is a number TODO: Add support for variables above
+    }
+    static bool isLeftAssociative(string input) {
+        if (input == "*" || input == "/" ||
+            input == "+" || input == "-") return true;
+        else return false;
+    }
+};
 
 template <class T>
 void inputBuilder(queue<T>& inputQueue) {
@@ -25,26 +46,6 @@ void inputBuilder(queue<T>& inputQueue) {
 template <class T>
 void postfixBuilder(queue<T>& inputQueue, queue<T>& postfixQueue) {
     stack<T> operatorStack;
-
-    /* Struct to help with postfixFunctions */
-    struct postfixHelper {
-        /* Returns precedence if input is operator, otherwise returns -1 */
-        static int isOperator(string input) {
-            if (input == "(")      {return 0;}
-            else if (input == ")") {return 0;}
-            else if (input == "^") {return 3;}
-            else if (input == "*") {return 2;}
-            else if (input == "/") {return 2;}
-            else if (input == "+") {return 1;}
-            else if (input == "-") {return 1;}
-            else { return -1;} // Input is a number TODO: Add support for variables above
-        }
-        static bool isLeftAssociative(string input) {
-            if (input == "*" || input == "/" ||
-                input == "+" || input == "-") return true;
-            else return false;
-        }
-    };
 
     while (!inputQueue.empty()) {
         string currentElement = inputQueue.front();
@@ -102,6 +103,45 @@ void printQueue(queue<T>& inputQueue) {
     } cout << endl;
 }
 
+template <class T>
+int postfixSolve(queue<T> inputQueue) {
+    stack<int> solutionStack;
+
+    while (!inputQueue.empty()) {
+        string currentElement = inputQueue.front();
+        cout << "Looking at: " << currentElement  << endl;
+        if (postfixHelper::isOperator(currentElement) == -1) { // Current element is #
+            solutionStack.push(atoi(currentElement.c_str()));
+        } else { // Current element is an operator
+
+            int number2 = solutionStack.top();
+            solutionStack.pop();
+            int number1 = solutionStack.top();
+            solutionStack.pop();
+
+            if (currentElement == "^") {
+                int temp = pow(number1, number2);
+                solutionStack.push(temp);
+            } else if (currentElement == "*") {
+                int temp = number1 * number2;
+                solutionStack.push(temp);
+            } else if (currentElement == "/") {
+                int temp = number1 / number2;
+                solutionStack.push(temp);
+            } else if (currentElement == "+") {
+                int temp = number1 + number2;
+                solutionStack.push(temp);
+            } else if (currentElement == "-") {
+                int temp = number1 - number2;
+                solutionStack.push(temp);
+            }
+        }
+        inputQueue.pop();
+    }
+    return solutionStack.top();
+}
+
+
 int main() {
     queue<string> inputQueue;
     queue<string> postfixQueue;
@@ -112,8 +152,9 @@ int main() {
 
     // Shunting yard
     postfixBuilder(inputQueue, postfixQueue);
-    printQueue(postfixQueue);
 
     // Calculate
+    answer = postfixSolve(postfixQueue);
+    cout << "Answer: " << answer << endl;
     return 0;
 }
