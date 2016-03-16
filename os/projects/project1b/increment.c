8e4ba1244ad121fa0f1a505d4ac66ad27abe4f21
Jonathan Poisson <Poisson.Jonathan777@gmail.com>
2016-03-16 17:42:29 -0400

    Complete part 1 OS project 1b

--- os/projects/project1b/increment.c
+++ os/projects/project1b/increment.c
@@ -3,13 +3,29 @@
 #include <string.h>
 
 int main(int argc, char * argv[]) {
+        FILE * fp; // File pointer
+        char buff[1024];
+        int lastNumberInFile;
+        int numberOfLinesToAdd;
+
 	if (argc != 3) {
 		printf("Incorrect number of arguments\n");
 		return 1;
 	} else { // argv[1] is number and argv[2] is the file name
 
-		FILE * fd; // File pointer
-		char buff[1024];
+                numberOfLinesToAdd = atoi(argv[1]); // Extract argument
+                fp = fopen(argv[2], "r+"); // Open file for reading and writing
+                while (fgets(buff, 1024,(FILE*)fp)) {
+                    if (feof(fp)) break;
+                    /*printf("%s", buff);*/
+                    lastNumberInFile = atoi(buff);
+                }
+                for (int i = 0; i < numberOfLinesToAdd; i++) {
+                    char str[3]; // Initialize new string to hold int
+                    sprintf(str, "%d\n", lastNumberInFile + (i + 1)); // Write to file
+                    fputs(str, fp);
+                }
+                return 0;
 
 
 	}
