88eed6bc351d0ce8607aadf0f39d232a3a279241
Big Brother <root@10.0.2.15>
2016-03-16 18:37:43 +0000

    Beginning of project 1b OS

---
+++ os/projects/project1b/increment.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char * argv[]) {
+	if (argc != 3) {
+		printf("Incorrect number of arguments\n");
+		return 1;
+	} else { // argv[1] is number and argv[2] is the file name
+
+		FILE * fd; // File pointer
+		char buff[1024];
+
+
+	}
+}
