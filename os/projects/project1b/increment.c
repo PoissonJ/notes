e58f9943251952e62fb1fbab0bf7deaf3e0bfbc7
Jonathan Poisson <Poisson.Jonathan777@gmail.com>
2016-03-19 13:26:23 -0400

    Redo of increment.c to meet Newman's specifications

--- os/projects/project1b/increment.c
+++ os/projects/project1b/increment.c
@@ -3,30 +3,47 @@
 #include <string.h>
 
 int main(int argc, char * argv[]) {
-        FILE * fp; // File pointer
-        char buff[1024];
-        int lastNumberInFile;
-        int numberOfLinesToAdd;
+    FILE * fp; // File pointer
+    char buff[1024];
+    int lastNumberInFile;
+    int numberOfLinesToAdd;
+    int numberToAdd;
+    int counter = 1;
 
-	if (argc != 3) {
-		printf("Incorrect number of arguments\n");
-		return 1;
-	} else { // argv[1] is number and argv[2] is the file name
+    if (argc != 3) {
+            printf("Incorrect number of arguments\n");
+            return 1;
+    } else { // argv[1] is number and argv[2] is the file name
 
-                numberOfLinesToAdd = atoi(argv[1]); // Extract argument
-                fp = fopen(argv[2], "r+"); // Open file for reading and writing
-                while (fgets(buff, 1024,(FILE*)fp)) {
-                    if (feof(fp)) break;
-                    /*printf("%s", buff);*/
-                    lastNumberInFile = atoi(buff);
-                }
-                for (int i = 0; i < numberOfLinesToAdd; i++) {
-                    char str[3]; // Initialize new string to hold int
-                    //sprintf(str, "%d\n", lastNumberInFile + (i + 1));
-                    fprintf(fp, "%d\n", lastNumberInFile + ( i + 1 )); // Write to file
-                }
-                return 0;
+        numberOfLinesToAdd = atoi(argv[1]); // Extract argument
 
+        /* Open file to initially get the last number before starting loop */
+        fp = fopen(argv[2], "r+"); // Open file for reading and writing
+        while (fgets(buff, 1024,(FILE*)fp)) {
+            if (feof(fp)) break;
+            lastNumberInFile = atoi(buff);
+        }
+        fclose(fp);
 
-	}
+        while (counter <= numberOfLinesToAdd) {
+            /* Computer Number to add */
+            numberToAdd = lastNumberInFile + counter;
+
+            fp = fopen(argv[2], "r+"); // Open file for reading and writing
+
+            /* Find last line */
+            while (fgets(buff, 1024,(FILE*)fp)) {
+                if (feof(fp)) break;
+            }
+
+            /* Append number to last line */
+            fprintf(fp, "%d\n", numberToAdd);
+
+            /* Close file */
+            fclose(fp);
+
+            counter++;
+        }
+        return 0;
+    }
 }
