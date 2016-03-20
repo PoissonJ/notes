637d21d3dd4896cebad6aba8aed75df1f2f3e6f9
Big Brother <root@10.0.2.15>
2016-03-20 08:00:12 +0000

    More work on safe increment...set_sv not working?

--- os/projects/project1b/safe_increment.c
+++ os/projects/project1b/safe_increment.c
@@ -3,12 +3,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 
 // Status variable for set and get functions. Not used but needed for implemented system call
 int status;
-int counter = 1;
 
-void criticalSection(char * fileName, bool isParent) {
+void criticalSection(char * fileName, int counter, int isParent) {
     FILE * incrementFile;
     char buff[1024];
     int lastNumberInFile;
@@ -25,14 +25,12 @@ void criticalSection(char * fileName, bool isParent) {
     int numberToAdd = lastNumberInFile + 1;
 
     /* Append number to last line */
-    if (isParent) fprintf(incrementFile, "Parent\n");
-    else fprintf(incrementFile, "child\n");
+    if (isParent) fprintf(incrementFile, "Parent %d\n", counter);
+    else fprintf(incrementFile, "child %d\n", counter);
     fprintf(incrementFile, "%d\n", numberToAdd);
 
     /* Close file */
     fclose(incrementFile);
-
-    counter++;
 }
 
 
@@ -43,7 +41,13 @@ int main(int argc, char * argv[]) {
     char fileName[1024];
     int process0 = 0; // First process listed in config file
     int process1 = 0; // Second process listed in config file
+    int parentCounter = 1;
+    int childCounter = 1;
     int numberOfLinesToAdd;
+    int returnStatus;
+    FILE * incrementFile;
+    char buff[1024];
+    int lastNumberInFile;
 
     /* Make sure program is run with correct # of arguments */
     if (argc != 4) {
@@ -69,16 +73,19 @@ int main(int argc, char * argv[]) {
         fprintf(configFile, "%ld\n", (long)idParent);
         fclose(configFile);
         printf("INFO: Creating and securing safe config file\n");
-        sleep(3); // Suspend so child process can write into config file
     } else {
-        sleep(2); // Suspend for a short time to let parent write into config
         configFile = fopen(argv[3], "a");
         fprintf(configFile, "%ld\n", (long)idChild);
         fclose(configFile);
     }
 
-    /* Read values from config file for uniformity */
-    while ((process0 == 0 && process1 == 0) || process0 == process1) {
+    /* Read values from config file for uniformity
+    *  While loop makes sure the file has been written
+    *  by both processes
+    */
+    
+    while (process0 == 0 || process1 == 0 ||
+           process0 == process1 || process0 > process1) {
         configFile = fopen(argv[3], "r");
         fgets(buff0, 1024, configFile); // Read process id for process 0
         fgets(buff1, 1024, configFile); // Read process id for process 1
@@ -92,36 +99,37 @@ int main(int argc, char * argv[]) {
         exit(1);
     }
 
-    if (idChild == 0)
-        printf("INFO: Parent begun incrementing %s safely\n", argv[2]);
-    else
-        printf("INFO: Child begun incrementing %s safely\n", argv[2]);
-
 
     if (idChild == 0){ // Parent process, process 0
         int shared;
         int lastNumberInFile;
         char flagAndTurn[3];
         flagAndTurn[2] = '\0';
+	
+        printf("INFO: Parent begun incrementing %s safely\n", argv[2]);
 
-        while (counter <= numberOfLinesToAdd) {
-            set_sv(11, &status); // Interested and turn belongs to process 1
+        while (parentCounter <= numberOfLinesToAdd) {
+            set_sv(3, &status); // Interested and turn belongs to process 1
+
             /* To avoid bitwise operations I check the value of shared literally */
             do {
                 int shared = get_sv(process1, &status);
+		//if (shared != 3) printf("%d\n", shared);
                 switch (shared) {
                     case 0: flagAndTurn[0] = '0'; flagAndTurn[1] = '0'; break;
                     case 1: flagAndTurn[0] = '0'; flagAndTurn[1] = '1'; break;
                     case 2: flagAndTurn[0] = '1'; flagAndTurn[1] = '0'; break;
                     case 3: flagAndTurn[0] = '1'; flagAndTurn[1] = '1'; break;
                 }
-            } while (flagAndTurn[0] == '1' && flagAndTurn[2] == '1');
+            } while (flagAndTurn[0] == '1' && flagAndTurn[1] == '1');
 
-            criticalSection(fileName, true);
+            criticalSection(fileName, parentCounter, 1);
+	    parentCounter++;
 
-            set_sv(1, &status);
+            set_sv(1, &status); // No longer interested and turn belongs to process 1
         }
 	printf("INFO: Parent done incrementing\n");
+	waitpid(process1, &returnStatus, 0);
     }
     else { // Child process, process 1
         int shared;
@@ -129,8 +137,11 @@ int main(int argc, char * argv[]) {
         char flagAndTurn[3];
         flagAndTurn[2] = '\0';
 
-        while (counter <= numberOfLinesToAdd) {
+        printf("INFO: Child begun incrementing %s safely\n", argv[2]);
+
+        while (childCounter <= numberOfLinesToAdd) {
             set_sv(2, &status); // Interested and turn belongs to process 0
+
             /* To avoid bitwise operations I check the value of shared literally */
             do {
                 int shared = get_sv(process0, &status);
@@ -140,14 +151,17 @@ int main(int argc, char * argv[]) {
                     case 2: flagAndTurn[0] = '1'; flagAndTurn[1] = '0'; break;
                     case 3: flagAndTurn[0] = '1'; flagAndTurn[1] = '1'; break;
                 }
-            } while (flagAndTurn[0] == '0' && flagAndTurn[2] == '0');
+            } while (flagAndTurn[0] == '0' && flagAndTurn[1] == '0');
 
-            criticalSection(fileName, false);
+            criticalSection(fileName, childCounter, 0);
 
-            set_sv(1, &status);
+            childCounter++;
+
+            set_sv(0, &status);
         }
 	printf("INFO: Child done incrementing\n");
+	waitpid(process0, &returnStatus, 0);
 
-    };
+    }
     return 0;
 }
